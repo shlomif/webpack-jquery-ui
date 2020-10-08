f6fe28e86d5098ff8a9b959b75b4ed166c397ea3
Shlomi Fish <shlomif@shlomifish.org>
2020-10-08 18:00:29 +0300

    try-tab-plugin: does not minimize

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('./main.css');
 require('jquery-ui/themes/base/core.css');
 require('jquery-ui/themes/base/menu.css');
-require('jquery-ui/themes/base/autocomplete.css');
+require('jquery-ui/themes/base/tabs.css');
 require('jquery-ui/themes/base/theme.css');
 var $ = require('jquery');
-var autocomplete = require('jquery-ui/ui/widgets/autocomplete');
+var tabs = require('jquery-ui/ui/widgets/tabs');
 
 $('<h1>Welcome to the programming languages quiz</h1>').appendTo('body');
-new autocomplete({
+new tabs({
   source: ['javascript', 'css', 'c', 'objectivec']
 }).element.appendTo('body').focus();
--- webpack-config.js
+++ webpack-config.js
@@ -1,32 +1,49 @@
 var Clean = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	entry: {
-		main: './index.js'
-	},
-	output: {
-		path: './dist',
-		filename: 'app.[hash].js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-			},
-			{
-				test: /\.(jpg|png)$/,
-				loader: 'file'
-			},
-		]
-	},
-	plugins: [
-		new Clean(['dist']),
-		new ExtractTextPlugin("app.[hash].css"),
-		new HtmlWebpackPlugin({
-			title: 'jQuery UI Autocomplete demo, built with webpack'
-		})
-	]
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                             include:/./,
+                             terserOptions: {
+                                 output: {
+                                     comments: false,
+                                 },
+                             },
+                             extractComments: false,
+            }),
+        ],
+    },
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: './dist',
+        filename: 'tot.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'file'
+            },
+        ]
+    },
+    mode: 'production',
+    plugins: [
+        new Clean(['dist']),
+        new ExtractTextPlugin("app.[hash].css"),
+        new HtmlWebpackPlugin({
+                              filename: 'index.html',
+                              title: 'jQuery UI Autocomplete demo, built with webpack'
+        })
+    ]
 };
